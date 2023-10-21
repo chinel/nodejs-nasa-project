ea9b8a99608b81ce04e8a732be9faa09413a4494
nelo <nelokwaz@gmail.com>
2023-10-21 11:34:59 +0100

    fix: update package json scripts command

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
-const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -9,7 +11,12 @@ app.use(
     origin: "http://localhost:3000",
   })
 ); // if you don't pass any option it allow all origins
+app.use(morgan("combined")); // combined is default format there are other formats, the common format is also used by Apache
 app.use(express.json()); // this parses json from incoming request
-app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use(planetsRouter);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,10 @@
+const launch = {
+  flightNumber: 100,
+  mission: "Keepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Keepler-442 b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
