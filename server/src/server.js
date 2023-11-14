a3eb66ad1e20376c5eb3ca2db307cb6ea2e61b44
nelo <nelokwaz@gmail.com>
2023-11-14 08:33:17 +0100

    feat: add dot env library and mongo db url

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,14 @@
 const http = require("http");
+require("dotenv").config();
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const MONGO_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@nasacluster.br0m6yy.mongodb.net/?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
