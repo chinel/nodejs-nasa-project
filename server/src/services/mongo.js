5e1d7a668608721f43da9e44fff12137c4bf7d9e
nelo <nelokwaz@gmail.com>
2023-11-28 06:48:22 +0100

    fix: move dotenv.config to a higher file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
