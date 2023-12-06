6e49cd75d8dfa7b987560b89ed974da015a8f9d7
nelo <nelokwaz@gmail.com>
2023-12-06 08:16:45 +0100

    feat: add passport logic

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,28 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const api = require("./routes/api");
 const auth = require("./routes/auth");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 const app = express();
+
+const config = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientId: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google Profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 app.use(helmet());
 app.use(
@@ -14,6 +34,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 ); // if you don't pass any option it allow all origins
+app.use(passport.initialize());
 app.use(morgan("combined")); // combined is default format there are other formats, the common format is also used by Apache
 app.use(express.json()); // this parses json from incoming request
 app.use(express.static(path.join(__dirname, "..", "public")));
