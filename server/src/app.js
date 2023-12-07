b8d4d6f19528d95f019bb0e300282e9c15d81962
nelo <nelokwaz@gmail.com>
2023-12-07 08:51:58 +0100

    fix: refactore auth logic

--- server/src/app.js
+++ server/src/app.js
@@ -5,28 +5,8 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const api = require("./routes/api");
 const auth = require("./routes/auth");
-const passport = require("passport");
-const { Strategy } = require("passport-google-oauth20");
 
 const app = express();
-
-const config = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-};
-
-const AUTH_OPTIONS = {
-  callbackURL: "/auth/google/callback",
-  clientId: config.GOOGLE_CLIENT_ID,
-  clientSecret: config.GOOGLE_CLIENT_SECRET,
-};
-
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log("Google Profile", profile);
-  done(null, profile);
-}
-
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 app.use(helmet());
 app.use(
@@ -35,7 +15,6 @@ app.use(
   })
 ); // if you don't pass any option it allow all origins
 
-app.use(passport.initialize());
 app.use(morgan("combined")); // combined is default format there are other formats, the common format is also used by Apache
 app.use(express.json()); // this parses json from incoming request
 app.use(express.static(path.join(__dirname, "..", "public")));
@@ -54,6 +33,9 @@ function checkLoggedIn(req, res, next) {
 }
 
 app.use("/auth", auth);
+app.use("failure", (req, res) => {
+  return res.send("Failed to log in!");
+});
 app.use("/*", checkLoggedIn, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 const auth = express.Router();
 
+const config = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientId: config.GOOGLE_CLIENT_ID,
+  clientSecret: config.GOOGLE_CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google Profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+app.use(passport.initialize());
 auth.get("/google", (req, res) => {});
-auth.get("/google/callback", (req, res) => {});
+auth.get("/google/callback", passport.authenticate("google"), {
+  failureRedirect: "/failure",
+  successRedirect: "/",
+});
 auth.get("/logout", (req, res) => {});
 
 module.exports = auth;
